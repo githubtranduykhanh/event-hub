24dd637d7c1fcf3aa10b60bb05a652b615034323
githubtranduykhanh <tranduykhanh.210101@gmail.com>
2024-10-03 22:40:39 +0700

    setup logo

--- src/screens/events/EventSeeAll.tsx
+++ src/screens/events/EventSeeAll.tsx
@@ -1,10 +1,25 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { ContainerComponent, SectionComponent, TextComponent } from '~/components'
+import { useFocusEffect } from '@react-navigation/native'
 
 const EventSeeAll = ({navigation,route}:any) => {
   const {filter}:{filter:string} = route.params
   console.log(filter)
+
+  useFocusEffect(
+    useCallback(() => {
+      // Logic hoặc hiệu ứng muốn thực hiện khi màn hình được focus
+      console.log('Screen is focused');
+      
+      // Trả về một hàm để clean-up khi màn hình bị unfocused (tương tự như trong useEffect)
+      return () => {
+        console.log('Screen is unfocused');
+      };
+    }, [])
+  );
+
+
   return (
     <ContainerComponent isScroll back title='Events'>
         <SectionComponent>
